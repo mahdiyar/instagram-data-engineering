0723c33db06e45542b596ae555fa8007d682a329
Justin Stewart <justinstewart.la@gmail.com>
2016-02-23 14:23:23 -0500

    wrote a quick script to test the instagram api connection

---
+++ instagram_test.py
@@ -0,0 +1,8 @@
+import cnfg
+from instagram.client import InstagramAPI
+
+config = cnfg.load(".instagram_config")
+api = InstagramAPI(client_id=config['CLIENT_ID'], client_secret=config['CLIENT_SECRET'])
+
+basics = api.user('22973081')
+print basics.id
