6d43ec2993f829f007bc28969d362b2724e7443b
Justin Stewart <justinstewart.la@gmail.com>
2016-02-26 18:07:42 -0500

    wrote function to pull down all needed media information, add helper functions to do exception handling for attribute errors when the feilds dont exist

--- models.py
+++ models.py
@@ -2,9 +2,10 @@ import cnfg
 from instagram.client import InstagramAPI
 
 config = cnfg.load(".instagram_config")
-api = InstagramAPI(client_id=config['CLIENT_ID'], client_secret=config['CLIENT_SECRET'])
+api = InstagramAPI(client_id=config['CLIENT_ID'], 
+				   client_secret=config['CLIENT_SECRET'])
 
-key_username = 'knowlita'
+
 
 def get_user_id(username):
 	''' Given a username, return the instagram user_id.
@@ -14,12 +15,13 @@ def get_user_id(username):
 	return user_search[0].id
 
 def get_user_basics(user_id):
-	''' Returns the instagram user object for a user_id'''
+	''' Returns a dictionary with instagram user data
+		stored for a given user_id'''
 
-	basics = {'user_id' : user_id}
 	user = api.user(user_id = user_id)
 
 	# Extract data and store in dict
+	basics = {'user_id' : user_id}
 	basics['username'] = user.username
 	basics['bio'] = user.bio
 	basics['followers_count']= user.counts['followed_by']
@@ -29,4 +31,57 @@ def get_user_basics(user_id):
 	return basics
 
 
-print get_user_basics(get_user_id(key_username))
+
+# try:
+# 	latitude = media.location.point.latitude
+# 	longitude = media.location.point.longitude
+# 	caption = media.caption.text
+# except:
+# 	pass
+
+##### Helper functions ######
+def get_latitude(media_object):
+	'''Exception handling for getting latitude.'''
+
+	try:
+		return media_object.location.point.latitude
+	except AttributeError:
+		return None
+
+def get_longitude(media_object):
+	'''Exception handling for getting longitude.'''
+	
+	try:
+		return media_object.location.point.longitude
+	except AttributeError:
+		return None
+
+def get_caption_text(media_object):
+	'''Exception handling for getting caption.'''
+
+	try:
+		return media_object.caption.text
+	except AttributeError:
+		return None
+
+def get_user_media(user_id):
+
+	media_list, next = api.user_recent_media(user_id = user_id)
+
+	media_data = {}
+	for media in media_list:
+		media_data[media.id] = {'likes_count' : media.like_count,
+								'comments_count' : media.comment_count,
+								'latitude' : get_latitude(media),
+								'longitude' : get_longitude(media),
+								'caption' : get_caption_text(media)}
+	return media_data
+
+
+
+
+
+key_username = 'knowlita'
+key_id = get_user_id(key_username)
+print get_user_media(key_id)
+
