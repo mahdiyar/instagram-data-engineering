e3dec3d0ffface9063c0344c65e3f376aa4b3081
Justin Stewart <justinstewart.la@gmail.com>
2016-02-26 18:09:20 -0500

    cleaned up packages

--- instagram_test.py
+++ instagram_test.py
@@ -1,6 +1,5 @@
 import cnfg
 from instagram.client import InstagramAPI
-from pprint import pprint 
 
 config = cnfg.load(".instagram_config")
 api = InstagramAPI(client_id=config['CLIENT_ID'], client_secret=config['CLIENT_SECRET'])
