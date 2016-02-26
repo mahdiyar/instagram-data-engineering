d23f923debf6d0c92daa5813d7c10f5f00e238ef
Justin Stewart <justinstewart.la@gmail.com>
2016-02-25 21:37:51 -0500

    editing test api call

--- instagram_test.py
+++ instagram_test.py
@@ -1,8 +1,9 @@
 import cnfg
 from instagram.client import InstagramAPI
+from pprint import pprint 
 
 config = cnfg.load(".instagram_config")
 api = InstagramAPI(client_id=config['CLIENT_ID'], client_secret=config['CLIENT_SECRET'])
 
-basics = api.user('22973081')
-print basics.id
+basics = api.user('332324252')
+pprint(basics.bio)
