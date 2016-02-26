8d16ba77b4364275657c9510eccb28248f38121d
Justin Stewart <justinstewart.la@gmail.com>
2016-02-26 16:53:42 -0500

    establishing the database conx

--- scrape_data.py
+++ scrape_data.py
@@ -1,5 +1,18 @@
 import cnfg
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from instagram.client import InstagramAPI
 
+from db_setup import User, Media, Follower
+
+# Setting the database connection.
+engine = create_engine('sqlite:///instagram.sqlite')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+# Configuring the instagram client
 config = cnfg.load(".instagram_config")
-api = InstagramAPI(client_id=config['CLIENT_ID'], client_secret=config['CLIENT_SECRET'])
+api = InstagramAPI(client_id=config['CLIENT_ID'], 
+				   client_secret=config['CLIENT_SECRET'])
+
