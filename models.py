895084428c615465a274d84111ffb9abf5c777e1
Justin Stewart <justinstewart.la@gmail.com>
2016-02-29 15:56:49 -0500

    added a try wrapper for both get basics and get media to account for private user profiles

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import cnfg
 from instagram.client import InstagramAPI
+from instagram.bind import InstagramAPIError
 
 config = cnfg.load(".instagram_config")
 api = InstagramAPI(client_id=config['CLIENT_ID'], 
@@ -18,32 +19,39 @@ def get_user_basics(user_id):
 	''' Returns a dictionary with instagram user data
 		stored for a given user_id'''
 
-	user = api.user(user_id = user_id)
+	try:
+		user = api.user(user_id = user_id)
+		# Extract data and store in dict
+		basics = {'user_id' : user_id}
+		basics['username'] = user.username
+		basics['bio'] = user.bio
+		basics['followers_count']= user.counts['followed_by']
+		basics['follows_count'] = user.counts['follows']
+		basics['media_count'] = user.counts['media']
 
-	# Extract data and store in dict
-	basics = {'user_id' : user_id}
-	basics['username'] = user.username
-	basics['bio'] = user.bio
-	basics['followers_count']= user.counts['followed_by']
-	basics['follows_count'] = user.counts['follows']
-	basics['media_count'] = user.counts['media']
-
-	return basics
-
-
+		return basics
+	except InstagramAPIError:
+		print 'This user is private'
+		return None
 
 def get_user_media(user_id):
 
-	media_list, next = api.user_recent_media(user_id = user_id)
+	try:
+		media_list, next = api.user_recent_media(user_id = user_id)
 
-	media_data = {}
-	for media in media_list:
-		media_data[media.id] = {'likes_count' : media.like_count,
-								'comments_count' : media.comment_count,
-								'latitude' : get_latitude(media),
-								'longitude' : get_longitude(media),
-								'caption' : get_caption_text(media)}
-	return media_data
+		media_data = {}
+		for media in media_list:
+			media_data[media.id] = {'likes_count' : media.like_count,
+									'comments_count' : media.comment_count,
+									'latitude' : get_latitude(media),
+									'longitude' : get_longitude(media),
+									'caption' : get_caption_text(media)}
+		return media_data
+
+	except InstagramAPIError:
+		print 'This user is private'
+		return None
+
 
 ##### Helper functions ######
 def get_latitude(media_object):
@@ -72,7 +80,12 @@ def get_caption_text(media_object):
 
 
 
-key_username = 'knowlita'
+##### Get an instagram user's list of follwers
+# followers, next = api.user_followed_by(user_id= user_id)
+
+
+key_username = 'emilyhk613'
 key_id = get_user_id(key_username)
 print get_user_media(key_id)
+
 
